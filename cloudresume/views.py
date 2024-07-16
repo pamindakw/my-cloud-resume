15953301c7ebf086aea73f77bd05ba9f5ceb392c
pamindakw <pamindaya@gmail.com>
2024-07-16 09:39:46 +0530

    Creating Base Template of resume

---
+++ cloudresume/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the cloud resume.")
+
+# Create your views here.
+
+def home(request):
+    return render(request, "base/home.html")
